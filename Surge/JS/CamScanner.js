820f2365594bbe93d2b6a172e3dad2f3617c90ab
sheltonzhu <498220739@qq.com>
2022-07-17 00:27:20 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -29,14 +29,13 @@ https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-r
 hostname = ap*.intsig.net
 // obj = {"data":{"psnl_vip_property":{"expiry":"1897017600"}},"server_time":"1687017600","pdfword_balance":"9000","login_ocr_balance":"1000","no_login_ocr_balance":"2","cert_mode_balance":9090,"bookmode_balance":29090,"immt_expy_points":"0","points":"1000","CamScanner_RoadMap":"0","used_points":"9090","ocr_balance":11,"fax_balance":"10100"},"ret":"0"};
 // obj.data.psnl_vip_property.expiry = "1897017601";
-
 **************************/
 let obj = JSON.parse($response.body);
 if (typeof obj === 'object') {
-  console.log(obj)
-  obj.data.psnl_vip_property.expiry = "1897017601";
+  console.log(JSON.stringify(obj))
 } else {
-  obj = {"data":{"psnl_vip_property":{"expiry":"1897017601"}}};
+  console.log(typeof obj)
 }
+obj = {"data":{"psnl_vip_property":{"expiry":"1897017601"}}};
 $done({body: JSON.stringify(obj)});
 
