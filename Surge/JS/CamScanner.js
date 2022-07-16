07edb2c695b8c399032766164a347f7d43c1f047
sheltonzhu <498220739@qq.com>
2022-07-17 00:13:32 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -28,9 +28,9 @@ https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-r
 [MITM]
 hostname = ap*.intsig.net
 // obj = {"data":{"psnl_vip_property":{"expiry":"1897017600"}},"server_time":"1687017600","pdfword_balance":"9000","login_ocr_balance":"1000","no_login_ocr_balance":"2","cert_mode_balance":9090,"bookmode_balance":29090,"immt_expy_points":"0","points":"1000","CamScanner_RoadMap":"0","used_points":"9090","ocr_balance":11,"fax_balance":"10100"},"ret":"0"};
+// obj.data.psnl_vip_property.expiry = "1897017601";
 
 **************************/
 let obj = JSON.parse($response.body);
-obj.data.psnl_vip_property.expiry = "1897017601";
-// obj = {"data":{"psnl_vip_property":{"expiry":"1897017601"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"1897017601"}}};
 $done({body: JSON.stringify(obj)});
