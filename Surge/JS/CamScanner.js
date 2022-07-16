15be4ea8b2325172164d1935d5e089e2654f6a6a
sheltonzhu <498220739@qq.com>
2022-07-17 01:10:03 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -34,17 +34,69 @@ let obj = JSON.parse($response.body);
 if (typeof obj === 'object') {
   if (obj.hasOwnProperty('data')){
     obj.data.psnl_vip_property = {"expiry":"1897017601"}
-    obj.data.points = "1000"
-    obj.data.ocr_balance = "1000"
-    obj.data.pdfword_balance = "1000"
-    obj.data.fax_balance = "1000"
-    obj.data.login_ocr_balance = "1000"
-    obj.data.no_login_ocr_balance = "1000"
-    obj.data.bookmode_balance = "1000"
-    obj.data.cert_mode_balance = "1000"
+    obj.data.points = "100000"
+    obj.data.ocr_balance = "100000"
+    obj.data.pdfword_balance = "100000"
+    obj.data.fax_balance = "100000"
+    obj.data.login_ocr_balance = "100000"
+    obj.data.no_login_ocr_balance = "100000"
+    obj.data.bookmode_balance = "100000"
+    obj.data.cert_mode_balance = "100000"
+    
+    obj.data.balance_demoire = "100000"
+    obj.data.watchad_vip_chance = "100000"
+    obj.data.vip_imagerestore_balance = 100000
+    obj.data.CamScanner_Erase = 100000
+    obj.data.trans_balance = "100000"
+    obj.data.upload_pdf_balance = "100000"
+    obj.data.excel_balance = "100000"
+    obj.data.patting_balance = "100000"
+    obj.data.watermarks_balance = "100000"
+    obj.data.pdfword_balance = "100000"
+    obj.data.imagerestore_balance = 100000
+    obj.data.bookmode_balance = 100000
+    
+    obj.data.greetcard_list = {
+            "greeting_card_2":"100000",
+            "greeting_card_3":"100000",
+            "greeting_card_4":"100000",
+            "greeting_card_5":"100000",
+            "greeting_card_6":"100000",
+            "greeting_card_9":"100000",
+            "greeting_card_10":"100000",
+            "greeting_card_11":"100000",
+            "CamScanner_PayGreetCard_1":"100000",
+            "CamScanner_PayGreetCard_2":"100000",
+            "CamScanner_PayGreetCard_3":"100000",
+            "CamScanner_PayGreetCard_4":"100000",
+            "CamScanner_PayGreetCard_5":"100000",
+            "CamScanner_PayGreetCard_6":"100000",
+            "CamScanner_NONVIP_PayGreetCard_1":"100000",
+            "CamScanner_NONVIP_PayGreetCard_2":"100000",
+            "CamScanner_NONVIP_PayGreetCard_3":"100000",
+            "greeting_card_1":"100000"
+        }
+    
+     obj.data.points_exchange_cfgrs = {
+            "CamScanner_ID_Card_Authenticity":0,
+            "CamScanner_ID_Card_Credit":0,
+            "CamScanner_CloudCap_1G":0,
+            "CamScanner_CertMode":0,
+            "CamScanner_Profile_Card_Format":0,
+            "CamScanner_Translation":0,
+            "CamScanner_Excel":0,
+            "CamScanner_Watermarks":0,
+            "CamScanner_CloudOCR":0,
+            "CamScanner_Pdf2excel":0,
+            "CamScanner_Pdf2ppt":0,
+            "CamScanner_Sign":0,
+            "CamScanner_Pdfword":0,
+            "CamScanner_AlbumImport":0
+        }
+    
     console.log("修改成功")
   } else {
-    obj = {"data":{"psnl_vip_property":{"expiry":"1897017601"}}};
+    obj = {"data":{"psnl_vip_property":{"expiry":"4110017601"}}};
   }
 } else {
   console.log(typeof obj)
