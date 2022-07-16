243206b06ab8d2ffa8cb352d1fab1894e25e0379
sheltonzhu <498220739@qq.com>
2022-07-17 02:18:17 +0800

    Update CamScanner2.js

--- Surge/JS/CamScanner2.js
+++ Surge/JS/CamScanner2.js
@@ -5,27 +5,10 @@ CamScanner 解锁部分高级特权
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/v3\.camscanner\.com\/app\/getPrivilegeItem url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner2.js
 
 [mitm]
-hostname = ap*.intsig.net
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response https:\/\/v3\.camscanner\.com\/app\/getPrivilegeItem requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/SheltonZhu/Script/master/Surge/JS/CamScanner2.js
-[MITM]
-hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-
-[MITM]
-hostname = ap*.intsig.net
+hostname = v3.camscanner.com
 
 **************************/
 let obj = JSON.parse($response.body);
