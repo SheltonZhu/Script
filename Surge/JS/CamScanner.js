10fc1f15b6f79eb2568d021dc32ec1d5e123872d
sheltonzhu <498220739@qq.com>
2022-07-16 23:53:52 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -30,6 +30,6 @@ hostname = ap*.intsig.net
 
 **************************/
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1687017600"}},"server_time":"1657986500","pdfword_balance":"0","login_ocr_balance":"10","no_login_ocr_balance":"2","cert_mode_balance":0,"bookmode_balance":20,"immt_expy_points":"0","points":"0","CamScanner_RoadMap":"0","used_points":"0","ocr_balance":11,"fax_balance":"0"},"ret":"0"}
+obj = {"data":{"psnl_vip_property":{"expiry":"1687017600"}},"server_time":"1687017600","pdfword_balance":"9000","login_ocr_balance":"1000","no_login_ocr_balance":"2","cert_mode_balance":9090,"bookmode_balance":29090,"immt_expy_points":"0","points":"1000","CamScanner_RoadMap":"0","used_points":"9090","ocr_balance":11,"fax_balance":"10100"},"ret":"0"}
 // obj = {"data":{"psnl_vip_property":{"expiry":"1687017600"}}};
 $done({body: JSON.stringify(obj)});
