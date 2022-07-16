96d99f97bc9ac27389dd078cfc6dd71535f07d20
sheltonzhu <498220739@qq.com>
2022-07-17 00:36:24 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -30,6 +30,7 @@ hostname = ap*.intsig.net
 // obj = {"data":{"psnl_vip_property":{"expiry":"1897017600"}},"server_time":"1687017600","pdfword_balance":"9000","login_ocr_balance":"1000","no_login_ocr_balance":"2","cert_mode_balance":9090,"bookmode_balance":29090,"immt_expy_points":"0","points":"1000","CamScanner_RoadMap":"0","used_points":"9090","ocr_balance":11,"fax_balance":"10100"},"ret":"0"};
 // obj.data.psnl_vip_property.expiry = "1897017601";
 **************************/
+console.log($response.body)
 let obj = JSON.parse($response.body);
 if (typeof obj === 'object') {
   console.log(JSON.stringify(obj))
